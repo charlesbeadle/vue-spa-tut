c22522fba86223ff7cb5affbfe04f5827b2064b6
Charles Beadle <charles.beadle@protonmail.com>
2022-09-03 09:26:19 -0400

    Apply dynamic routing lesson changes

--- src/router/index.js
+++ src/router/index.js
@@ -1,23 +1,30 @@
-import { createRouter, createWebHistory } from "vue-router";
-import EventList from "../views/EventList.vue";
-import AboutView from "../views/AboutView.vue";
+import { createRouter, createWebHistory } from 'vue-router'
+import EventList from '../views/EventList.vue'
+import EventDetails from '../views/EventDetails.vue'
+import AboutView from '../views/AboutView.vue'
 
 const routes = [
   {
-    path: "/",
-    name: "home",
-    component: EventList,
+    path: '/',
+    name: 'EventList',
+    component: EventList
   },
   {
-    path: "/about",
-    name: "about",
-    component: AboutView
+    path: '/event/:id',
+    name: 'EventDetails',
+    props: true,
+    component: EventDetails
   },
-];
+  {
+    path: '/about',
+    name: 'AboutView',
+    component: AboutView
+  }
+]
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
-  routes,
-});
+  routes
+})
 
-export default router;
+export default router
